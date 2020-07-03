2847ce0c136a6764bd7486344d6df3205d03e8c8
Tobias Speer <tspeer@ls-speer>
2020-07-03 13:27:10 +0200

    20200703 add exporter

---
+++ xnc-export.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+vm=""
+vm_id=`cat $vm | cut -d ' ' -f1`
+vm_name=`cat $vm | cut -d ' ' -f2 | sed 's/\ /_/g`
+
+for vm in `cat vm-list`;do
+    echo "$vm wird exportiert..."
+    xe vm-export vm="`${vm_id}`" filename="${vm_name}.xen"
+    
+done
+echo Alle VMs exportiert
+exit
