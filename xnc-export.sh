8bfbace7460c49768dd4a8048679311f5a874269
Tobias Speer <tspeer@ls-speer>
2020-07-03 15:12:43 +0200

    small errors debugged

--- xnc-export.sh
+++ xnc-export.sh
@@ -4,7 +4,7 @@ vm=""
 vm_id=`cat $vm | cut -d ' ' -f1`
 vm_name=`cat $vm | cut -d ' ' -f2 | sed 's/\ /_/g`
 
-for vm in `cat vm-list`;do
+for $vm in `cat vm-list`;do
     echo "$vm wird exportiert..."
     xe vm-export vm="`${vm_id}`" filename="${vm_name}.xen"
     
