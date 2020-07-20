513940a045b3f422c07b541a24cd94c125b8fc83
Tobias Speer <tspeer@ls-speer>
2020-07-20 10:14:51 +0200

    kleiner Anpassungen

--- xnc-export.sh
+++ xnc-export.sh
@@ -4,10 +4,14 @@ vm=""
 vm_id=`cat $vm | cut -d ' ' -f1`
 vm_name=`cat $vm | cut -d ' ' -f2 | sed 's/\ /_/g`
 
-for $vm in `cat vm-list`;do
+for $vm in `cat vm-list`;
+do
+    echo "$vm wird herunter gefahren..."
+    xe vm-shutdown uuid="`${vm_id}`" force=true
+    # aus
     echo "$vm wird exportiert..."
-    xe vm-export vm="`${vm_id}`" filename="${vm_name}.xen"
-    
+    xe vm-export uuid="`${vm_id}`" filename="${vm_name}.xen"
+    echo "Export von $vm erfolgreich beendet"
 done
 echo Alle VMs exportiert
 exit
